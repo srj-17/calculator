a42911b900c6b06cc212c04bb427a4823d17f34a
srj-17 <saugatrijal17@gmail.com>
2024-06-25 15:17:37 +0545

    Logic is wrong somewhere, repeat again

--- script.js
+++ script.js
@@ -2,9 +2,13 @@
 const calculator = document.querySelector('.calculator');
 const display = document.querySelector('.display');
 const keyContainer = document.querySelector('.key-container');
-let firstOperand;
-let secondOperand;
-let displayValue;
+const numKeys = Array.from(document.querySelectorAll('.num-key'));
+const operatorKeys = Array.from(document.querySelectorAll('.operator-key'));
+const extraKeys = Array.from(document.querySelectorAll('.x-key'));
+let firstOperand = null;
+let secondOperand = null;
+let displayValue = '';
+let memValue = [];
 
 const add = function(firstOperand, secondOperand) {
     return firstOperand + secondOperand;
@@ -26,28 +30,84 @@ const operate = function (operator, firstOperand, secondOperand) {
     let result;
     switch (operator) {
         case "+":
-            result = add(firstOperand, secondOperand);   
+            result = add(+firstOperand, +secondOperand);   
             break;
         
         case "-":
-            result = subtract(firstOperand, secondOperand);
+            result = subtract(+firstOperand, +secondOperand);
             break;
         
         case "/":
-            result = divide(firstOperand, secondOperand);
+            result = divide(+firstOperand, +secondOperand);
             break;
 
         case "*":
-            result = multiply(firstOperand, secondOperand);
+            result = multiply(+firstOperand, +secondOperand);
             break;
 
         default:
             break;
     }
-    return result;
+    return String(result);
 }
 
 keyContainer.addEventListener('click', (event) => {
-    displayValue = event.target.value || '';
-    display.innerText += displayValue;
+    if (event.target.value in numKeys) {
+        if (memValue.length === 3) {
+            displayValue = memValue.shift;
+        } else {
+            if (memValue.at(-1) in operatorKeys) {
+                memValue = memValue.unshift()
+            }
+            displayValue = displayValue.concat(event.target.value);
+        } 
+        display.textContent = displayValue;
+    } else if (event.target.value in operatorKeys) {
+        if (memValue.length === 3) {
+            displayValue = operate(memValue[1], parseFloat(memValue[0]),
+                           parseFloat(memValue[2]));
+            if (event.target.value === '=') {
+                display.textContent = displayValue;
+                memValue = [].concat(displayValue);
+            } else {
+                display.textContent = displayValue;
+                memValue = [displayValue, event.target.value, ''];
+                // last value pops out next time numeric value is pressed
+            }
+        } else { // if (memValue.length < 3) 
+            if (event.target.value === '=') {
+                display.textContent = 'Not Calculable';
+            } else {
+                memValue = memValue.concat(event.target.value);
+            }
+        }
+    } else { // in extraKeys
+        switch(event.target.value) {
+            case '.':
+                if (displayValue === '') {
+                    displayValue = '.';
+                    display.textContent = displayValue;
+                } else {
+                    displayValue = displayValue.concat(event.target.value);
+                    display.textContent = displayValue;
+                }
+            case 'clear':
+                display.textContent = '';
+                memValue = [];
+            case 'sign':
+                if (display === ''){
+                    displayValue = displayValue.concat(event.target.textContent);
+                    // don't display, we're just keeping it for the next number
+                } else {
+                    if (Math.sign(displayValue)) { // in case displayValue doesn't already have some special character except number like "-"    
+                        if (Math.sign(displayValue) === 1) {
+                            displayValue = String(-displayValue);
+                        } else if (Math.sign(displayValue) === -1) { 
+                            displayValue = String(Math.abs(displayValue));
+                        } 
+                    }
+                }
+        }
+    }
 });
+
