f8b0a88ef3539150c63bdac4dba5dd2fd39e14a6
srj-17 <saugatrijal17@gmail.com>
2024-06-25 12:09:09 +0545

    Add display feature for each button pressed
    Enables the user to know which key he pressed, also
    we can keep track of which operands to operate on

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // calculator operations
-firstOperand = 4;
-secondOperand = 4;
+const calculator = document.querySelector('.calculator');
+const display = document.querySelector('.display');
+const keys = document.querySelector('.keys');
+let firstOperand;
+let secondOperand;
+let displayValue;
 
 const add = function(firstOperand, secondOperand) {
     return firstOperand + secondOperand;
@@ -18,7 +22,7 @@ const multiply = function(firstOperand, secondOperand) {
     return firstOperand * secondOperand;
 };
 
-const operate(operator, firstOperand, secondOperand) {
+const operate = function (operator, firstOperand, secondOperand) {
     let result;
     switch (operator) {
         case "+":
@@ -36,8 +40,14 @@ const operate(operator, firstOperand, secondOperand) {
         case "*":
             result = multiply(firstOperand, secondOperand);
             break;
-            
+
         default:
             break;
     }
+    return result;
 }
+
+keys.addEventListener('click', (event) => {
+    displayValue = event.target.value || '';
+    display.innerText += displayValue;
+});
